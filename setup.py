60f35109267bb5a83189fe63ac225af9a6ac2c67
Omar Gutiérrez <omar.gup@gmail.com>
2022-05-11 19:45:32 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()    
+
 setup(
-    name="walkingffp",
-    version="0.0.1",
+    name="movingfp",
+    version="0.1.0",
     license='MIT',
-    author="Omar Gutiérrez",
+    author="Network and Data Science Laboratory CIC-IPN México",
     author_email="omar.gup@gmail.com",
-    description="Moving/Walking Fire Fighter Problem Generator",
-    long_description="Instances generator of the Moving/Walking Fire Fighter Problem, proposed by the Network and Data Science Laboratory CIC-IPN México.",
+    description="Moving Fire Fighter Problem Generator",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/omargup/firefighter_problem_generator",
-    keywords='walking firefighter problem',
-    packages=["walkingffp"],
+    keywords='moving firefighter problem',
+    packages=["movingfp"],
     python_requires='>=3.8',
     install_requires=['numpy', 'networkx'])
